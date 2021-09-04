bdbd5273d49543ac956c5b87ad80002f6a855fd9
Danejw <djw.learn@gmail.com>
2021-09-04 11:50:02 -0700

    Camera + Dragon Assets
    Added a new dragon asset into the scene.
    
    Added Cinemachine into the project and connected it to follow the dragon.
    
    Created a character controller to move the dragon around the scene.
    
    Placed the mastercanvas on top of the dragon in world space and made it always look at the camera.

---
+++ Assets/FaceCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    public Transform trans;
+    private Transform body;
+
+    private void Start()
+    {
+        body = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (trans)
+            body.LookAt(trans);
+    }
+}
---
+++ Assets/KeyboardControllerInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControllerInput : MonoBehaviour
+{
+    CharacterController controller;
+    public float speed = 2;
+    public float rotationalSpeed = 1;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        Rotate();
+        Move();
+    }
+
+    private void Move()
+    {
+        float verticalMove = Input.GetAxis("Vertical");
+
+        // Moves the character forward based of its local rotations
+        Vector3 move = transform.forward * verticalMove;
+        move = move.normalized;
+        var forwardAmount = Vector3.Dot(transform.forward, move);
+
+        // Move slower if moving backwards
+        if (forwardAmount > 0)
+            controller.Move(speed * Time.deltaTime * move);
+        else if (forwardAmount < 0)
+            controller.Move((speed/4) * Time.deltaTime * move);
+    }
+
+    private void Rotate()
+    {
+        float horizontalRotation = Input.GetAxis("Horizontal");
+
+        transform.localEulerAngles += new Vector3(0, horizontalRotation * rotationalSpeed, 0);
+    }
+}
